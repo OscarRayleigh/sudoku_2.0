866df3f2868815f046c8ea752d0342f9be3fa258
Vincent Toscas <vince@MacBook-Air-de-MACBOOK.local>
2021-01-12 13:14:02 +0100

    Not workign yet

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 import random
+global grid
 
-def show_tab(tab_9x9):
+import sys
+
+sys.setrecursionlimit(10**6)
+
+
+
+def show_grid(grid_9x9):
     k = 0
     print("      0     1     2  |  3     4     5  |  6     7     8")
     for i in range(0,9):
         if i == 0 or i == 3 or i == 6:
             print("   -----------------------------------------------------")
-        print(k, " | ",tab_9x9[i][0], " | ", tab_9x9[i][1]," | ", tab_9x9[i][2]
-        ," | ",tab_9x9[i][3], " | ", tab_9x9[i][4]," | ", tab_9x9[i][5]," | "
-        ,tab_9x9[i][6], " | ", tab_9x9[i][7]," | ", tab_9x9[i][8])
+        print(k, " | ",grid_9x9[i][0], " | ", grid_9x9[i][1]," | ", grid_9x9[i][2]
+        ," | ",grid_9x9[i][3], " | ", grid_9x9[i][4]," | ", grid_9x9[i][5]," | "
+        ,grid_9x9[i][6], " | ", grid_9x9[i][7]," | ", grid_9x9[i][8])
         k+=1
 
 def flatten(big_list):
@@ -21,25 +28,22 @@ def flatten(big_list):
 
 def import_game():
 
-    tab = []
+    grid = []
     temp = []
-    big_tab = []
+    big_grid = []
     final_grid = []
     with open('sudoku.txt', 'r') as f:
         for line in f:
             temp.append(line)
-
-
-
     k = 10
     del temp[0] # #Supprime le GRID (le 1er)
-    del temp[k-1::k] #Supprime le GRID
+    del temp[k-1::k] #Supprime les autres grids
 
     for string in temp:
-        big_tab.append(string[:-1]) # Supprime le saut de ligne
+        big_grid.append(string[:-1]) # Supprime les sauts de ligne
 
     first_el = random.randint(0, 49)
-    random_grid = big_tab[first_el*9:first_el*9+9]
+    random_grid = big_grid[first_el*9:first_el*9+9]
 
 
     temp = []
@@ -47,8 +51,6 @@ def import_game():
     for i in random_grid:
         for row in i:
             temp.append(row)
-
-
     for x in range(len(temp)):
         temp[x] = int(temp[x])
 
@@ -63,17 +65,69 @@ def split(grid):
 
     return final_grid
 
-def test():
-    tab = [[0,0,3,0,2,0,6,0,0],
-    [9,0,0,3,0,5,0,0,1],
-    [0,0,1,8,0,6,4,0,0],
-    [0,0,8,1,0,2,9,0,0],
-    [7,0,0,0,0,0,0,0,8],
-    [0,0,6,7,0,8,2,0,0],
-    [0,0,2,6,0,9,5,0,0],
-    [8,0,0,2,0,3,0,0,9],
-    [0,0,5,0,1,0,3,0,0]]
+
+def estpossible(grid,y,x,n):
+    temp = []
+    l = 0
+    # On vérifie les rows
+    for i in grid[y]:
+        if i == n:
+            return False
+    # On vérifie les colonnes
+    for j in range(0,9):
+        temp.append(grid[l][x])
+        l+=1
+
+    for p in temp:
+        if n == p:
+            return False
+    return True
+
+
+def resoudre():
+    if not 0 in flatten(grid):
+        show_tab(grid)
+        return True
+    r = list(range(1,10))
+    random.shuffle(r)
+    for y in range(0,9):
+        for x in range(0,9):
+            if not 0 in flatten(grid):
+                show_tab(grid)
+                return True
+            if grid[y][x] == 0:
+                for n in r:
+                    if estpossible(grid,y,x,n):
+                        grid[y][x] = n
+                        resoudre()
+    return False
+
+
+
+
+
+
+def new_value(tab):
+    try:
+        user_input = input("Que voulez vous changer ? xy:valeur    : \n")
+        user_input = user_input.split(":")
+        temp = str(user_input[0])
+        coo_x = temp[0]
+        coo_y = temp[1]
+        value = int(user_input[1])
+        if value <= 0 or value > 9:
+            print("Veuillez entrer des valeurs de 1 à 9 inclus")
+            new_value(tab)
+        tab[int(coo_y)][int(coo_x)] = value
+    except:
+        print("Erreur la donnée entrée n'a pas pu être enregistrée")
+        new_value(tab)
     return tab
 
-tab = test()
-show_tab(tab)
+
+
+
+
+grid = import_game()
+while not resoudre():
+    resoudre()
